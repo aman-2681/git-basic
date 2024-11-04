dad92ed1202e2421327ed0289f6d2879823dc92d
Aman Kumar <ak17701725@gmail.com>
2024-11-04 17:01:51 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Hello from './components/Hello'
 import './App.css'
 const App = () => {
   return (
-    <Hello/>
+    <>
+        <Hello/>
+        <h1>World</h1>
+    </>
   )
 }
-
 export default App
